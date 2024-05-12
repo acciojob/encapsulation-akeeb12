9bf237958f82e9a52d32f35fbe907ef092a85208
Akeeb1 <akeeebmalligawad605@gmail.com>
2024-05-12 14:30:23 +0530

    Encapsulation-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+import com.driver.RWOnly;
+
 public class Main {
-  
+  public static void main (String[] args)
+  {
+      RWOnly rwOnlyObj = new RWOnly();
+
+      rwOnlyObj.setName("Akeeb MM");
+      System.out.println("Name:" + rwOnlyObj.getName());
+  }
 }
